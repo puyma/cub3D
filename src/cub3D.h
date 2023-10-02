840cf6f0c5dd7da4aeb50adc3148cf846c04332a
puyma <mpuig-ma@student.42barcelona.com>
2023-10-02 13:12:58 +0200

    init header

---
+++ src/cub3D.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3D.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/02 13:06:12 by mpuig-ma          #+#    #+#             */
+/*   Updated: 2023/10/02 13:11:19 by mpuig-ma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <fcntl.h>		// open ,
+# include <unistd.h>	// close, read, write
+# include <sys/types.h>	// write
+# include <sys/uio.h>	// write
+# include <stdio.h>		// printf, perror, strerror
+# include <stdlib.h>	// malloc, free, exit
+# include <math.h>		// all funcs. of the math library // -lm
+
+# include "mlx.h"
+
+typedef struct s_data
+{
+	int			game;
+}				t_data;
+
+#endif /* cub3D.h */
