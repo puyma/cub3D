edd9df4eca80f9cc948fd61cfb7873db1c463ea8
puyma <mpuig-ma@student.42barcelona.com>
2023-10-11 17:42:26 +0200

    uncomment mlx_loop()

--- src/ft_load_game.c
+++ src/ft_load_game.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 10:33:58 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/10/11 15:29:33 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/10/11 17:42:11 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ int	ft_start_game(t_game *game)
 	mlx_hook(game->mlx_window, ON_DESTROY, 0, &ft_destroy, (void *) game);
 	mlx_hook(game->mlx_window, ON_KEYDOWN, 0, &ft_keycode, (void *) game);
 	mlx_mouse_hide();
-	//mlx_loop(game->mlx);
+	mlx_loop(game->mlx);
 	return (EXIT_SUCCESS);
 }
