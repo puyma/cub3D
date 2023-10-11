d8c62b3b6db24da22c4b15f0bf7df8f258f9d43c
puyma <mpuig-ma@student.42barcelona.com>
2023-10-11 15:05:08 +0200

    format

--- src/main.c
+++ src/main.c
@@ -6,35 +6,29 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 12:12:24 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/10/11 09:44:10 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/10/11 15:04:48 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-static int	ft_init_main(int argc, char **argv);
-static int	ft_init_t_game(t_game *game, char **argv);
+static int	ft_check_args(int argc, char **argv);
 
 int	main(int argc, char **argv)
 {
 	t_game	game;
 
+	if (ft_check_args(argc, argv) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
 	game.map_filename_ptr = argv[1];
-	if (ft_init_main(argc, argv) == EXIT_FAILURE)
-		return (EXIT_FAILURE);
-	if (ft_init_t_game(&game, argv) == EXIT_FAILURE)
-		return (EXIT_FAILURE);
 	if (ft_load_game(&game) == EXIT_FAILURE)
-	{
-		ft_fprintf(stderr, "Error ft_load_game()\n");
-		return (ft_clean(&game), EXIT_FAILURE);
-	}
-	printf("here\n");
-	ft_clean(&game);
-	return (EXIT_SUCCESS);
+		return (EXIT_FAILURE + 1);
+	if (ft_start_game(&game) == EXIT_FAILURE)
+		return (EXIT_FAILURE + 2);
+	return (ft_clean(&game), EXIT_SUCCESS);
 }
 
-static int	ft_init_main(int argc, char **argv)
+static int	ft_check_args(int argc, char **argv)
 {
 	if (argc != 2)
 		return (EXIT_FAILURE);
@@ -44,32 +38,14 @@ static int	ft_init_main(int argc, char **argv)
 		exit(EXIT_SUCCESS);
 	}
 	if (*argv[1] == '\0' || ft_strlen(argv[1]) >= _POSIX_PATH_MAX)
-		return (ft_fprintf(stderr, "%s: %s\n",
-				EXEC_NAME, "File name too long"), EXIT_FAILURE);
-	if (ft_endswith(argv[1], MAP_EXT) == EXIT_FAILURE)
-		return (ft_fprintf(stderr, "%s: %s\n",
-				EXEC_NAME, "Invalid file name extension"), EXIT_FAILURE);
-	return (EXIT_SUCCESS);
-}
-
-static int	ft_init_t_game(t_game *game, char **argv)
-{
-	int		fd;
-	t_list	*file;
-
-	game->i_load_cueue = NULL;
-	game->mlx = NULL;
-	game->mlx_window = NULL;
-	game->map = NULL;
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
-		return (ft_fprintf(stderr, "%s: %s: %s\n",
-				EXEC_NAME, argv[1], strerror(errno)), EXIT_FAILURE);
-	file = ft_read_file(fd);
-	if (file == NULL)
+	{
+		ft_fprintf(stderr, "%s: %s\n", EXEC_NAME, "File name too long");
 		return (EXIT_FAILURE);
-	if (ft_load_map(file, game) == EXIT_FAILURE)
-		return (ft_clean_map(game->map), EXIT_FAILURE);
-	close(fd);
+	}
+	if (ft_endswith(argv[1], MAP_EXT) == EXIT_FAILURE)
+	{
+		ft_fprintf(stderr, "%s: %s\n", EXEC_NAME, "Invalid file extension");
+		return (EXIT_FAILURE);
+	}
 	return (EXIT_SUCCESS);
 }
