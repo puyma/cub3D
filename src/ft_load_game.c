6ae3f736b28281451872c5e02a31e94146a06c5f
puyma <mpuig-ma@student.42barcelona.com>
2023-10-11 17:40:39 +0200

    rename load_game to start_game

--- src/ft_load_game.c
+++ src/ft_load_game.c
@@ -6,13 +6,13 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 10:33:58 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/10/11 09:45:22 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/10/11 15:29:33 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-int	ft_load_game(t_game *game)
+int	ft_start_game(t_game *game)
 {
 	game->mlx = mlx_init();
 	if (game->mlx == NULL || ft_load_textures(game) == EXIT_FAILURE)
@@ -22,6 +22,6 @@ int	ft_load_game(t_game *game)
 	mlx_hook(game->mlx_window, ON_DESTROY, 0, &ft_destroy, (void *) game);
 	mlx_hook(game->mlx_window, ON_KEYDOWN, 0, &ft_keycode, (void *) game);
 	mlx_mouse_hide();
-	mlx_loop(game->mlx);
+	//mlx_loop(game->mlx);
 	return (EXIT_SUCCESS);
 }
