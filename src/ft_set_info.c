bf20f3db2bab86cb64e1642e1b1b9bb9d9a5eeff
puyma <mpuig-ma@student.42barcelona.com>
2023-10-13 13:18:51 +0200

    patch leaks

--- src/ft_set_info.c
+++ src/ft_set_info.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 17:00:44 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/10/13 11:27:35 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/10/13 11:48:18 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ static int	ft_set_info_value_path(char *s, t_game *game)
 
 	values = ft_split(s, 040);
 	if (ft_arrlen(values) != 2)
-		return (EXIT_FAILURE);
+		return (ft_free_arr(values), EXIT_FAILURE);
 	if (ft_strcmp("NO", values[0]) == 0)
 		game->i_north.path_to_image_file = ft_strdup(values[1]);
 	else if (ft_strcmp("SO", values[0]) == 0)
@@ -60,7 +60,7 @@ static int	ft_set_info_value_path(char *s, t_game *game)
 		game->i_east.path_to_image_file = ft_strdup(values[1]);
 	game->i_load_cueue[i] = *(values[0]);
 	++i;
-	return (EXIT_SUCCESS);
+	return (ft_free_arr(values), EXIT_SUCCESS);
 }
 
 static int	ft_set_info_value_color(char *s, t_game *game)
@@ -73,7 +73,7 @@ static int	ft_set_info_value_color(char *s, t_game *game)
 	ft_striteri(s, &ft_replace_ispunct);
 	values = ft_split(s, 040);
 	if (ft_arrlen(values) != 4)
-		return (EXIT_FAILURE);
+		return (ft_free_arr(values), EXIT_FAILURE);
 	color_values = values + 1;
 	if (color_values == NULL || ft_arrlen(color_values) != 3)
 		exit_status = EXIT_FAILURE;
@@ -83,5 +83,5 @@ static int	ft_set_info_value_color(char *s, t_game *game)
 	else if (ft_strcmp("P", values[0]) == 0
 		&& ft_set_rgb_color(color_values, &game->p_color) == EXIT_FAILURE)
 		exit_status = EXIT_FAILURE;
-	return (exit_status);
+	return (ft_free_arr(values), exit_status);
 }
