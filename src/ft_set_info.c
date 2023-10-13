21ad7838f59b755a3a43838bd842f8265c48a618
puyma <mpuig-ma@student.42barcelona.com>
2023-10-13 11:41:17 +0200

    mv functions

--- src/ft_set_info.c
+++ src/ft_set_info.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 17:00:44 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/10/13 10:47:45 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/10/13 11:27:35 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 static int	ft_set_info_value_path(char *values, t_game *game);
 static int	ft_set_info_value_color(char *values, t_game *game);
-static int	ft_fill_map(t_list *map_lst, t_map *map);
 
 int	ft_set_info(t_list *info, t_game *game)
 {
@@ -85,47 +84,4 @@ static int	ft_set_info_value_color(char *s, t_game *game)
 		&& ft_set_rgb_color(color_values, &game->p_color) == EXIT_FAILURE)
 		exit_status = EXIT_FAILURE;
 	return (exit_status);
-}
-
-int	ft_set_board(t_list *map_lst, t_game *game)
-{
-	size_t	i;
-
-	game->map = ft_calloc(1, sizeof(t_map));
-	if (game->map == NULL)
-		return (EXIT_FAILURE);
-	game->map->width = ft_lstwidth(map_lst);
-	game->map->height = ft_lstheight(map_lst);
-	game->map->board = (int **) ft_calloc(game->map->width, sizeof(int *));
-	if (game->map->board == NULL)
-		return (free(game->map), EXIT_FAILURE);
-	i = 0;
-	while (i < game->map->width)
-		game->map->board[i++] = ft_calloc(game->map->height, sizeof(int));
-	if (ft_fill_map(map_lst, game->map) == EXIT_FAILURE)
-		return (EXIT_FAILURE);
-	return (EXIT_SUCCESS);
-}
-
-static int	ft_fill_map(t_list *map_lst, t_map *map)
-{
-	char	*str;
-	size_t	x;
-	size_t	y;
-
-	y = 0;
-	while (map_lst != NULL)
-	{
-		x = 0;
-		str = map_lst->content;
-		while (x < map->width && str[x] != '\0')
-		{
-			if (ft_strchr(C_ALLOWED, str[x]) != NULL || str[x] == '1')
-				map->board[x][y] = str[x];
-			++x;
-		}
-		++y;
-		map_lst = map_lst->next;
-	}
-	return (EXIT_SUCCESS);
 }
