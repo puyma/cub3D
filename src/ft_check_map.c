bd158b8abd339baf9236d26b123265a336b39819
puyma <mpuig-ma@student.42barcelona.com>
2023-10-19 12:40:05 +0200

    OK: solved through homogenization

--- src/ft_check_map.c
+++ src/ft_check_map.c
@@ -6,22 +6,25 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 16:02:30 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/10/19 10:14:48 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/10/19 12:36:50 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-#define EX_F EXIT_FAILURE
+#define COL		0
+#define ROW		1
+#define CURRENT	0
+#define PREV	1
+#define FAIL	EXIT_FAILURE
 
-/** WIP **/
+/** WIP: refactorization **/
 
 static int	ft_check_up(t_map *map);
 static int	ft_check_down(t_map *map);
 static int	ft_check_left(t_map *map);
 static int	ft_check_right(t_map *map);
-static int	ft_check_row(int **board, size_t y, size_t from, size_t to);
-static int	ft_check_column(int **board, size_t x, size_t from, size_t to);
+static int	ft_check_range(int **board, int type, size_t pos, size_t range[2]);
 
 int	ft_check_map(t_map *map)
 {
@@ -36,144 +39,132 @@ int	ft_check_map(t_map *map)
 	return (EXIT_SUCCESS);
 }
 
+static int	ft_check_range(int **board, int type, size_t pos, size_t range[2])
+{
+	size_t	from;
+	size_t	to;
+
+	from = range[0];
+	to = range[1];
+	if (range [0] > range[1])
+	{
+		from = range[1];
+		to = range[0];
+	}
+	while (from-- > to + 1)
+	{
+		if (type == COL && board[pos][from] != '1')
+			return (EXIT_FAILURE);
+		if (type == ROW && board[from][pos] != '1')
+			return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
+
 static int	ft_check_left(t_map *map)
 {
+	size_t	range[2];
 	size_t	y;
-	size_t	left;
-	size_t	prev_left;
 	int		sub;
 
 	y = 0;
-	prev_left = 0;
+	range[PREV] = 0;
 	while (y < map->height)
 	{
-		left = 0;
-		while (left < map->width && map->board[left][y] != '1')
-			++left;
-		sub = left - prev_left;
+		range[CURRENT] = 0;
+		while (range[CURRENT] < map->width
+			&& map->board[range[CURRENT]][y] != '1')
+			++range[CURRENT];
+		sub = range[CURRENT] - range[PREV];
 		if (y != 0 && (sub > 1 || sub < -1))
 		{
-			if (ft_check_row(map->board, y, left, prev_left) == EXIT_FAILURE
-				&& ft_check_row(map->board, y - 1, left, prev_left) == EX_F)
-				return (ft_fprintf(stderr, "LEFT\n"), EXIT_FAILURE);
+			if (ft_check_range(map->board, ROW, y, range) == FAIL
+				&& ft_check_range(map->board, ROW, y - 1, range) == FAIL)
+				return (ft_fprintf(stderr, "LEFT\n"), FAIL);
 		}
 		++y;
-		prev_left = left;
+		range[PREV] = range[CURRENT];
 	}
 	return (EXIT_SUCCESS);
 }
 
 static int	ft_check_right(t_map *map)
 {
+	size_t	range[2];
 	size_t	y;
-	size_t	right;
-	size_t	prev_right;
 	int		sub;
 
 	y = 0;
-	prev_right = 0;
+	range[PREV] = 0;
 	while (y < map->height)
 	{
-		right = map->width - 1;
-		while (right > 0 && map->board[right][y] != '1')
-			--right;
-		sub = right - prev_right;
+		range[CURRENT] = map->width - 1;
+		while (range[CURRENT] > 0
+			&& map->board[range[CURRENT]][y] != '1')
+			--range[CURRENT];
+		sub = range[CURRENT] - range[PREV];
 		if (y != 0 && (sub > 1 || sub < -1))
 		{
-			if (ft_check_row(map->board, y, right, prev_right) == EXIT_FAILURE
-				&& ft_check_row(map->board, y - 1, right, prev_right) == EX_F)
-				return (ft_fprintf(stderr, "RIGHT\n"), EXIT_FAILURE);
+			if (ft_check_range(map->board, ROW, y, range) == FAIL
+				&& ft_check_range(map->board, ROW, y - 1, range) == FAIL)
+				return (ft_fprintf(stderr, "RIGHT\n"), FAIL);
 		}
 		++y;
-		prev_right = right;
+		range[PREV] = range[CURRENT];
 	}
 	return (EXIT_SUCCESS);
 }
 
 static int	ft_check_up(t_map *map)
 {
+	size_t	range[2];
 	size_t	x;
-	size_t	up;
-	size_t	prev_up;
 	int		sub;
 
 	x = 0;
-	prev_up = 0;
+	range[PREV] = 0;
 	while (x < map->width)
 	{
-		up = 0;
-		while (up < map->height && map->board[x][up] != '1')
-			++up;
-		sub = up - prev_up;
+		range[CURRENT] = 0;
+		while (range[CURRENT] < map->height
+			&& map->board[x][range[CURRENT]] != '1')
+			++range[CURRENT];
+		sub = range[CURRENT] - range[PREV];
 		if (x != 0 && (sub > 1 || sub < -1))
 		{
-			if (ft_check_column(map->board, x, up, prev_up) == EXIT_FAILURE
-				&& ft_check_column(map->board, x - 1, up, prev_up) == EX_F)
-				return (ft_fprintf(stderr, "UP\n"), EXIT_FAILURE);
+			if (ft_check_range(map->board, COL, x, range) == FAIL
+				&& ft_check_range(map->board, COL, x - 1, range) == FAIL)
+				return (ft_fprintf(stderr, "UP\n"), FAIL);
 		}
 		++x;
-		prev_up = up;
+		range[PREV] = range[CURRENT];
 	}
 	return (EXIT_SUCCESS);
 }
 
 static int	ft_check_down(t_map *map)
 {
+	size_t	range[2];
 	size_t	x;
-	size_t	down;
-	size_t	prev_down;
 	int		sub;
 
 	x = 0;
-	prev_down = 0;
+	range[PREV] = 0;
 	while (x < map->width)
 	{
-		down = map->height - 1;
-		while (down > 0 && map->board[x][down] != '1')
-			--down;
-		sub = down - prev_down;
+		range[CURRENT] = map->height - 1;
+		while (range[CURRENT] > 0
+			&& map->board[x][range[CURRENT]] != '1')
+			--range[CURRENT];
+		sub = range[CURRENT] - range[PREV];
 		if (x != 0 && (sub > 1 || sub < -1))
 		{
-			if (ft_check_column(map->board, x, down, prev_down) == EXIT_FAILURE
-				&& ft_check_column(map->board, x - 1, down, prev_down) == EX_F)
-				return (ft_fprintf(stderr, "DOWN\n"), EXIT_FAILURE);
+			if (ft_check_range(map->board, COL, x, range) == FAIL
+				&& ft_check_range(map->board, COL, x - 1, range) == FAIL)
+				return (ft_fprintf(stderr, "DOWN\n"), FAIL);
 		}
 		++x;
-		prev_down = down;
-	}
-	return (EXIT_SUCCESS);
-}
-
-static int	ft_check_row(int **board, size_t y, size_t from, size_t to)
-{
-	if (from > to)
-	{
-		while (from-- > to + 1)
-			if (board[from][y] != '1')
-				return (EXIT_FAILURE);
-	}
-	else
-	{
-		while (to-- > from + 1)
-			if (board[to][y] != '1')
-				return (EXIT_FAILURE);
-	}
-	return (EXIT_SUCCESS);
-}
-
-static int	ft_check_column(int **board, size_t x, size_t from, size_t to)
-{
-	if (from > to)
-	{
-		while (from-- > to + 1)
-			if (board[x][from] != '1')
-				return (EXIT_FAILURE);
-	}
-	else
-	{
-		while (to-- > from + 1)
-			if (board[x][to] != '1')
-				return (EXIT_FAILURE);
+		range[PREV] = range[CURRENT];
 	}
 	return (EXIT_SUCCESS);
 }
