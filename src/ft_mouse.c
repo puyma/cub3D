407ed7e83b6d4ce052937acba0bf6b5cb75f7b2e
puyma <mpuig-ma@student.42barcelona.com>
2023-10-19 17:05:01 +0200

    proper ft_mouse arguments

--- src/ft_mouse.c
+++ src/ft_mouse.c
@@ -6,15 +6,20 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 10:08:03 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/10/19 10:10:12 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/10/19 17:03:27 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-int	ft_mouse(t_game *game)
+int	ft_mouse(int x, int y, void *param)
 {
-	(void) game;
-	printf("mouse\n");
+	t_game	*game;
+
+	game = param;
+	if (x < 0 || y < 0 || x > WIN_WIDTH || y > WIN_HEIGHT)
+		return (mlx_mouse_show(), EXIT_SUCCESS);
+	mlx_mouse_hide();
+	ft_printf("mouse %d, %d\n", x, y);
 	return (EXIT_SUCCESS);
 }
