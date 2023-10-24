25e289e528cb7d4d1a9a15b544ad5bc599aaec90
puyma <mpuig-ma@student.42barcelona.com>
2023-10-24 13:14:53 +0200

    render background, minimap, hand(held) item

--- src/ft_render.c
+++ src/ft_render.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 12:37:20 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/10/24 11:17:07 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/10/24 13:03:09 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,41 +22,87 @@ void	ft_mlx_pixel_put(t_imgdata *img, int x, int y, int color)
 
 /** WIP: use an already (always de same) t_imgdata to write to **/
 
-void	ft_fill_background(t_game *game)
-{
-	t_imgdata	*img = &game->i_main_frame;
-	size_t		x;
-	size_t		y;
+#define START	0
+#define END		1
 
-	img->img = mlx_new_image(game->mlx, WIN_WIDTH, WIN_HEIGHT);
-	img->address = mlx_get_data_addr(img->img, &img->bits_per_pixel,
-			&img->line_length, &img->endian);
-	y = 0;
-	while (y < game->win_height)
+void	ft_draw_quadrangle_range(t_imgdata *img, size_t range_x[2],
+		size_t range_y[2], int color)
+{
+	size_t	x;
+	size_t	y;
+
+	y = range_y[START];
+	while (y < range_y[END])
 	{
-		x = 0;
-		while (x < game->win_width)
-		{
-			if (y < (game->win_height / 2))
-				ft_mlx_pixel_put(img, x++, y, game->c_color.argb);
-			else
-				ft_mlx_pixel_put(img, x++, y, game->f_color.argb);
-		}
+		x = range_x[START];
+		while (x < range_x[END])
+			ft_mlx_pixel_put(img, x++, y, color);
 		++y;
 	}
-	mlx_put_image_to_window(game->mlx, game->mlx_window, img->img, 0, 0);
-	free(img->img);
+}
+
+void	ft_draw_quadrangle_coordinates(t_imgdata *i, size_t coord_x[2],
+		size_t coord_y[2], int color)
+{
+	size_t	x;
+	size_t	y;
+
+	y = coord_y[START];
+	while (y < coord_y[END])
+	{
+		x = coord_x[START];
+		while (x < coord_x[END])
+			ft_mlx_pixel_put(i, x++, y, color);
+		++y;
+	}
+}
+
+void	ft_add_background(t_imgdata *img, t_game *game)
+{
+	static size_t		range_x[2];
+	static size_t		range_y[2];
+
+	range_x[START] = 0;
+	range_x[END] = game->win_width;
+	range_y[START] = 0;
+	range_y[END] = game->win_height / 2;
+	ft_draw_quadrangle_range(img, range_x, range_y, game->c_color.argb);
+	range_y[START] = range_y[END];
+	range_y[END] = game->win_height;
+	ft_draw_quadrangle_range(img, range_x, range_y, game->f_color.argb);
+}
+
+void	ft_add_minimap(t_imgdata *img, t_game *game)
+{
+	static size_t	x[2];
+	static size_t	y[2];
+
+	(void) game;
+	x[START] = PIX_SIZE / 4;
+	y[START] = x[START];
+	x[END] = PIX_SIZE * 4;
+	y[END] = PIX_SIZE * 3;
+	ft_draw_quadrangle_coordinates(img, x, y, MINIMAP_COLOR);
+}
+
+void	ft_add_handheld(t_imgdata *img, t_game *game)
+{
+	(void) img;
+	(void) game;
 }
 
 int	ft_render(t_game *game)
 {
-	t_imgdata	*img;
+	t_imgdata	*i;
 
-	img = &game->i_main_frame;
-	ft_fill_background(game);
-	// write to window
-	(void) game;
-	//mlx_put_image_to_window(game->mlx, game->mlx_window, img->img, 0, 0);
-	write(STDOUT_FILENO, "w\n", 2);
+	i = &game->i_main_frame;
+	i->img = mlx_new_image(game->mlx, WIN_WIDTH, WIN_HEIGHT);
+	i->address = mlx_get_data_addr(i->img, &(i->bits_per_pixel),
+			&(i->line_length), &(i->endian));
+	ft_add_background(i, game);
+	ft_add_minimap(&game->i_main_frame, game);
+	ft_add_handheld(&game->i_main_frame, game);
+	mlx_put_image_to_window(game->mlx, game->mlx_window, i->img, 0, 0);
+	free(i->img);
 	return (EXIT_SUCCESS);
 }
