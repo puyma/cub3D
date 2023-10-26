35555872614cbb8be34ca39a876490aea0fad0df
puyma <mpuig-ma@student.42barcelona.com>
2023-10-26 17:30:26 +0200

    declare test function

--- src/cub3D.h
+++ src/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 13:06:12 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/10/24 13:13:55 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/10/26 17:30:05 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -147,6 +147,7 @@ int						ft_load_textures(t_game *game);
 int						ft_keycode(int keycode, t_game *game);
 int						ft_mouse(int x, int y, void *game);
 int						ft_destroy(t_game *game);
+void					ft_rays(t_game *game);
 
 //						array utils
 size_t					ft_arrlen(char **array);
