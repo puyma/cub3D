62e8d29603df184561cc9c160bf54f3631a0fe71
puyma <mpuig-ma@student.42barcelona.com>
2023-11-03 13:24:54 +0100

    minimap_background

--- src/minimap.c
+++ src/minimap.c
@@ -6,13 +6,27 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 12:04:20 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/11/03 12:58:37 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/11/03 13:24:24 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
+static void	minimap_background(t_imgdata *img, t_game *game);
+
 void	minimap(t_imgdata *img, t_game *game)
+{
+	static int	x[2];
+	static int	y[2];
+
+	x[START] = PIX_SIZE / 4;
+	y[START] = x[START];
+	x[END] = PIX_SIZE * 4;
+	y[END] = PIX_SIZE * 3;
+	minimap_background(img, game);
+}
+
+static void	minimap_background(t_imgdata *img, t_game *game)
 {
 	static int	x[2];
 	static int	y[2];
