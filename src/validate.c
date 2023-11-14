604250516270b06427bdec5e92fe39249c9cfcb0
puyma <mpuig-ma@student.42barcelona.com>
2023-11-14 17:26:28 +0100

    norm

--- src/validate.c
+++ src/validate.c
@@ -6,18 +6,19 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 17:15:26 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/11/14 17:15:32 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/11/14 17:25:49 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
+//check theres only one player (at least)
+//check theres one line for each NO SO EA WE F C
+//check map characters
+
 int	validate_map(t_list *map_lst)
 {
 	(void) map_lst;
-	//check theres only one player (at least)
-	//check theres one line for each NO SO EA WE F C
-	//check map characters
 	return (EXIT_SUCCESS);
 }
 
